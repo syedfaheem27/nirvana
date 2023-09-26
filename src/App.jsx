a564e3bf9d4303231de299cde40fb60f3331b7b5
syedfaheem27 <syedfaheem2719@gmail.com>
2023-09-26 20:04:17 +0530

    Add react-hot-toast for notifications

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import Bookings from "./pages/Bookings";
 
 import { GlobalStyles } from "./styles/globalStyles";
 import AppLayout from "./ui/AppLayout";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -42,6 +43,28 @@ const App = () => {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{
+          margin: "8px",
+        }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)",
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 };
