03af0836b5dfd884d94e8023bbbcc86357e08f82
syedfaheem27 <syedfaheem2719@gmail.com>
2023-10-08 16:00:03 +0530

    Add a logout functionality

---
+++ src/features/authentication/LogOut.jsx
@@ -0,0 +1,15 @@
+import { HiArrowRightOnRectangle } from "react-icons/hi2";
+import ButtonIcon from "../../ui/ButtonIcon";
+import { useLogout } from "./useLogout";
+import SpinnerMini from "../../ui/SpinnerMini";
+
+const LogOut = () => {
+  const { isLoggingOut, logout } = useLogout();
+  return (
+    <ButtonIcon disabled={isLoggingOut} onClick={logout}>
+      {!isLoggingOut ? <HiArrowRightOnRectangle /> : <SpinnerMini />}
+    </ButtonIcon>
+  );
+};
+
+export default LogOut;
